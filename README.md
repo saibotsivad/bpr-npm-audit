1559bb55248f771a6bc9903f397eb5329fb05b83
Tobias Davis <accounts@davistobias.com>
2020-05-05 12:33:56 -0500

    feat: init with the basics

--- README.md
+++ README.md
@@ -1,2 +1,79 @@
-# bp-reports-npm-audit
-Bitbucket Pipelines report for "npm audit".
+# bpr-npm-audit
+
+Bitbucket Pipelines added [reports](https://confluence.atlassian.com/bitbucket/code-insights-994316785.html) as a feature in pull requests.
+
+With this module, you can get the results of `npm audit` as a report, with *zero configuration*, using `npx`:
+
+```yaml
+pipelines:
+  my-pipeline:
+    - step:
+        script:
+          - npx bpr-npm-audit
+```
+
+## security
+
+This module has zero dependencies (outside of NodeJS), and is simple enough to audit yourself.
+
+If you are very paranoid, I recommend forking this repository, auditing the forked code, and then using `npx` pointed to your fork:
+
+```yaml
+pipelines:
+  my-pipeline:
+    - step:
+        script:
+          - npx @username/bpr-npm-audit
+```
+
+(Where `username` is your Github username.)
+
+## configure
+
+Parameters are passed in as environment variables. For example:
+
+```yaml
+pipelines:
+  my-pipeline:
+    - step:
+        script:
+          - BPR_NAME="My Report" BPR_ID="myid" BPR_LEVEL="low" npx bpr-npm-audit
+```
+
+### proxy
+
+Configure by setting the environment variable `BPR_PROXY` to one of these options.
+
+* `local` - (default) used in normal Pipelines
+* `pipe` - used in custom pipes
+
+### report name
+
+Configure by setting the environment variable `BPR_NAME`.
+
+Default: `Security: npm audit`
+
+### report id
+
+Configure by setting the environment variable `BPR_ID`.
+
+Default: `npmaudit`
+
+### fail condition
+
+Configure by setting the environment variable `BRP_LEVEL` to one of these options.
+
+* `low`
+* `moderate`
+* `high` (the default)
+* `critical`
+
+If there are any vulnerabilities at that level or higher, the report will be marked as failed.
+
+## license
+
+This project is published and released under the [Very Open License](http://veryopenlicense.com).
+
+---
+
+(Made with ❤️ by [Tobias Davis](https://davistobias.com).)
---
+++ index.js
@@ -0,0 +1,99 @@
+const { spawnSync } = require('child_process')
+
+const ORDERED_LEVELS = [
+	'info',
+	'low',
+	'moderate',
+	'high',
+	'critical'
+]
+
+const PROXY_TYPES = {
+	local: 'http://localhost:29418',
+	pipe: 'http://host.docker.internal:29418'
+}
+
+const bitbucket = {
+	branch: process.env.BITBUCKET_BRANCH,
+	commit: process.env.BITBUCKET_COMMIT,
+	build: process.env.BITBUCKET_BUILD_NUMBER,
+	owner: process.env.BITBUCKET_REPO_OWNER,
+	slug: process.env.BITBUCKET_REPO_SLUG
+}
+if (Object.keys(bitbucket).filter(key => bitbucket[key]).length !== Object.keys(bitbucket).length) {
+	console.error('Not all Bitbucket environment variables were set.')
+	process.exit(1)
+}
+
+const reportName = process.env.BPR_NAME || 'Security: npm audit'
+const reportId = process.env.BPR_ID || 'npmaudit'
+const proxyUrl = PROXY_TYPES[process.env.BPR_PROXY || 'local']
+const auditLevel = process.env.BPR_LEVEL || 'high'
+if (!ORDERED_LEVELS.includes(auditLevel)) {
+	console.error('Unsupported audit level.')
+	process.exit(1)
+}
+if (!proxyUrl) {
+	console.error('Unsupported proxy configuration.')
+	process.exit(1)
+}
+
+const startTime = new Date().getTime()
+const { stderr, stdout } = spawnSync('npm', [ 'audit', '--json' ])
+if (stderr.toString()) {
+	console.error('Could not execute the `npm audit` command.', stderr.toString())
+	process.exit(1)
+}
+const audit = JSON.parse(stdout.toString())
+
+const highestLevelIndex = ORDERED_LEVELS.reduce((value, level, index) => {
+	return audit.metadata.vulnerabilities[level]
+		? index
+		: value
+}, -1)
+
+const report = {
+	title: reportName,
+	details: 'Results of npm audit.',
+	report_type: 'SECURITY',
+	reporter: bitbucket.owner,
+	result: highestLevelIndex <= ORDERED_LEVELS.indexOf(auditLevel)
+		? 'FAILED'
+		: 'PASSED',
+	data: [
+		{
+			title: 'Duration (seconds)',
+			type: 'DURATION',
+			value: Math.round((new Date().getTime() - startTime) / 1000)
+		},
+		{
+			title: 'Dependencies',
+			type: 'NUMBER',
+			value: audit.metadata.dependencies
+		},
+		{
+			title: 'Safe to merge?',
+			type: 'BOOLEAN',
+			value: highestLevelIndex <= ORDERED_LEVELS.indexOf(auditLevel)
+		}
+	]
+}
+
+const url = [
+	'http://api.bitbucket.org/2.0/repositories/',
+	bitbucket.owner,
+	'/',
+	bitbucket.slug,
+	'/commit/',
+	bitbucket.commit,
+	'/reports/',
+	reportName
+].join('')
+
+const response = spawnSync('curl', [
+	'--proxy', proxyUrl,
+	'--request', 'PUT',
+	url,
+	'--header', 'Content-Type: application/json',
+	'--data-raw', `'${JSON.stringify(report)}'`
+])
